a205e59eff0ae3f229a551c8f2972893803bbd0e
gerburgos <68874865+gerburgos@users.noreply.github.com>
2020-08-12 22:05:46 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -22,7 +22,7 @@ function onSignIn(googleUser) {
 
     console.log("ID Token: " + id_token);
 
-    location.href = "https://gerburgos.github.io/repo_eMercado/index.html"
+     location.href = "https://gerburgos.github.io/repo_eMercado/index.html"
 
 }
 
