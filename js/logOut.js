3de195908ac8f675823e6cbfd1ca20e07a5b1f2b
gerburgos <68874865+gerburgos@users.noreply.github.com>
2020-10-10 23:11:49 -0300

    Update

---
+++ js/logOut.js
@@ -0,0 +1,17 @@
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+        designSign();       
+    });
+}
+
+function onLoad() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init();
+    });
+}
+
+function deleteUser() {
+    localStorage.removeItem('user');
+    localStorage.removeItem('img');
+}
